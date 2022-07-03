d11b523f9f8fe36390b18bda8fe7137a36149d5c
luizatorrens <luiza.torrens.2006@gmail.com>
2022-07-03 20:20:39 -0300

    projeto integrador c/ bootstrap

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import HeaderComp from "./components/HeaderComp.vue";
+</script>
+<template>
+  <HeaderComp />
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<style>
+@import "@/assets/style.css";
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+</style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        <nav class="nav nav-pills nav-justified">
+                <span class="nav-link links"> <RouterLink to="/autor">Autor</RouterLink> </span>
+                <span class="nav-link links"> <RouterLink to="/editora">Editora</RouterLink> </span>
+                <span class="nav-link links"> <RouterLink to="/categoria">Categoria</RouterLink> </span>
+                <span class="nav-link links"> <RouterLink to="/livros">Livros</RouterLink> </span>
+        </nav>
+    </header>
+</template>
+<style>
+.links {
+    text-decoration: none;
+}
+</style>
